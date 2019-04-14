dea785d2c23ad05e4abe2fe117b7984ee05a5801
pateldarsh14 <43825079+pateldarsh14@users.noreply.github.com>
2019-04-13 19:44:06 -0500

    updated script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ function init() {
   
   eb.addEventListener('click', function() {
     alert('Darsh Patel: ' + msg.value);
-    document.getElementById('textoutput').innerHTML = msg.value; });
+    document.getElementById('textoutput').innerHTML = msg.value;});
 }
 window.addEventListener('load', init);
