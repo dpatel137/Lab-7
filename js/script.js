29d26bda4a34594fe6668370a36251da9ba7ca90
pateldarsh14 <43825079+pateldarsh14@users.noreply.github.com>
2019-04-13 19:38:18 -0500

    updated spacing

--- js/script.js
+++ js/script.js
@@ -5,7 +5,5 @@ function init() {
   eb.addEventListener('click', function() {
     alert('Darsh Patel: ' + msg.value);
     document.getElementById('textoutput').innerHTML = msg.value; });
-  	
 }
-
 window.addEventListener('load', init);
